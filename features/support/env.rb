c83ac93e871701b5ee8ab9b38a096ca40ac67f0e
ozan <ozan.guzel@digiturk.com.tr>
2024-11-18 11:26:11 +0300

    last_commit

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,6 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'capybara/dsl'
 require 'rspec'
-require 'allure-cucumber'
-require 'report_builder'
 
 
 Dir["#{Dir.pwd}/features/util/**/*.rb"].each { |file| require file }
@@ -16,10 +14,4 @@ Capybara.configure do |config|
   config.default_selector = :css
   config.default_max_wait_time = 40
   config.default_driver = :selenium_chrome
-end
-
-AllureCucumber.configure do |config|
-  config.results_directory = 'reports/allure-results' # Test sonuçlarının kaydedileceği dizin
-  config.clean_results_directory = true # Her çalıştırmadan önce eski sonuçları temizler
-  config.logging_level = Logger::INFO # Log seviyesini ayarlar
 end
